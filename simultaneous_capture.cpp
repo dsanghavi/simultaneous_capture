e22ecaaa8c660c3e16760d59265832711e8d8f1a
Dhruv <dhruv.joshi.1989@gmail.com>
2015-10-23 17:50:00 +0530

    Working. Timestamp verification required. Please make an images folder for the program to run succesfully.

--- simultaneous_capture.cpp
+++ simultaneous_capture.cpp
@@ -1,13 +1,22 @@
 #include <boost/thread.hpp>
 #include <boost/thread/barrier.hpp>
 #include <boost/bind.hpp>
-#include <boost/atomic.hpp>
+//#include <boost/atomic.hpp>
 #include "FlyCapture2.h"
 #include <vector>
+#include <sstream>
+#include <boost/lexical_cast.hpp>
 
 boost::mutex io_mutex;
 
 using namespace FlyCapture2;
+
+unsigned long getThreadId(){
+    std::string threadId = boost::lexical_cast<std::string>(boost::this_thread::get_id());
+    unsigned long threadNumber = 0;
+    sscanf(threadId.c_str(), "%lx", &threadNumber);
+    return threadNumber;
+}
 
 void PrintCameraInfo( CameraInfo* pCamInfo )
 {
@@ -34,100 +43,57 @@ void PrintError( Error error )
     error.PrintErrorTrace();
 }
 
-void thread_fun(boost::barrier& cur_barier, boost::atomic<int>& current)
+/*void thread_fun(boost::barrier& cur_barier, boost::atomic<int>& current)
 {
     ++current;
     cur_barier.wait();
     //boost::lock_guard<boost::mutex> locker( );
     std::cout << current << std::endl;
-}
+}*/
 
 void capture_images(boost::barrier& cur_barier, Camera* c){
+    Error error;
     CameraInfo camInfo;
     c->GetCameraInfo( &camInfo );
-    PrintCameraInfo(&camInfo);
-    /*
+    //boost::lock_guard<boost::mutex> locker( );
+    //PrintCameraInfo(&camInfo);
+    int threadID = (int)getThreadId();
     Image rawImage, convertedImage; 
     unsigned int numImages = 50;
-    if (argc < 2) {
-      printf("No parameter entered for number of images, going with default 50.\n");
-    } else {
-      // numImages = (int)*argv[1];
-      numImages = 1;
-    }
-    std::vector<Image> vecImages1;
-        vecImages1.resize(numImages);
-    std::vector<Image> vecImages2;
-        vecImages2.resize(numImages);
+    
+    std::vector<Image> vecImages;
+    vecImages.resize(numImages);
     for (unsigned int j=0; j < numImages; j++ ) {
-        for (unsigned int cam=0; cam < numCameras; cam++) {
-
-        error = pcam[cam]->RetrieveBuffer( &rawImage );
+        cur_barier.wait();
+        error = c->RetrieveBuffer( &rawImage );
         if (error != PGRERROR_OK)
         {
           PrintError( error );
-          continue;
+          continue; //For threads, all the other threads have to continue -------------
         }
-
         // We'll also print out a timestamp
-            TimeStamp timestamp = rawImage.GetTimeStamp();
-                printf(
-                  "Cam %d - Frame %d - TimeStamp [%d %d]\n",
-                  cam,
-                  j,
-                  timestamp.cycleSeconds,
-                  timestamp.cycleCount
-        );
-
-        if(cam==0) {
-            vecImages1[j].DeepCopy(&rawImage);
-        } else {
-            vecImages2[j].DeepCopy(&rawImage);
-        }
-        }
+        TimeStamp timestamp = rawImage.GetTimeStamp();
+        printf("Cam %d - Frame %d - TimeStamp [%d %d]\n",threadID,j,timestamp.cycleSeconds,timestamp.cycleCount);
+        vecImages[j].DeepCopy(&rawImage);
     }
     //Process and store the images captured
     for (unsigned int j=0; j < numImages; j++) {
-        error = vecImages1[j].Convert( PIXEL_FORMAT_RGB, &convertedImage );
-                if (error != PGRERROR_OK)
-                {
-                  PrintError( error );
-                  return -1;
-                }
-
-                // Create a unique filename
-                char filename[512];
-                sprintf( filename, "./images/cam--%d-%d.tiff", 0, j);
-
-                // Save the image. If a file format is not passed in, then the file
-                // extension is parsed to attempt to determine the file format.
-                error = convertedImage.Save( filename );
-                if (error != PGRERROR_OK)
-                {
-                  PrintError( error );
-                  return -1;
-                }
-        //Do the same for the second camera
-        error = vecImages2[j].Convert( PIXEL_FORMAT_RGB, &convertedImage );
-                if (error != PGRERROR_OK)
-                {
-                  PrintError( error );
-                  return -1;
-                }
-
-                // Create a unique filename
-                char filename2[512];
-                sprintf( filename2, "./images/cam--%d-%d.tiff", 1, j);
-
-                // Save the image. If a file format is not passed in, then the file
-                // extension is parsed to attempt to determine the file format.
-                error = convertedImage.Save( filename2 );
-                if (error != PGRERROR_OK)
-                {
-                  PrintError( error );
-                  return -1;
-                }
-    }*/
+        error = vecImages[j].Convert( PIXEL_FORMAT_RGB, &convertedImage );
+        if (error != PGRERROR_OK){
+          PrintError( error );
+          return;
+        }
+        // Create a unique filename
+        char filename[512];
+        sprintf( filename, "./images/cam--%d-%d.tiff", threadID, j);
+        // Save the image. If a file format is not passed in, then the file
+        // extension is parsed to attempt to determine the file format.
+        error = convertedImage.Save( filename );
+        if (error != PGRERROR_OK){
+          PrintError( error );
+          return;
+        }
+    }
 }
 
 int main()
@@ -191,12 +157,13 @@ int main()
 //call the threads to capture images, convert and save to file------------------------------
 
     boost::barrier bar(2);
-    boost::atomic<int> current(0);
+   // boost::atomic<int> current(0);
     boost::thread thr1(boost::bind(&capture_images, boost::ref(bar), boost::ref(pcam[0])));
-    //boost::thread thr2(boost::bind(&capture_images, boost::ref(bar), boost::ref(pcam[1])));
+    boost::thread thr2(boost::bind(&capture_images, boost::ref(bar), boost::ref(pcam[1])));
     //boost::thread thr2(boost::bind(&thread_fun, boost::ref(bar), boost::ref(current)));
     thr1.join();
-    //thr2.join();
+    thr2.join();
+    printf( "Do not press any key till images are captured and written...\n" );
     getchar();
     for ( unsigned int i = 0; i < numCameras; i++ )
     {
